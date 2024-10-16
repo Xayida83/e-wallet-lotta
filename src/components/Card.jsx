1def655737f445ca030481475c77351301728ed9
Xayida83 <lotta.lindberg83@gmail.com>
2024-10-16 15:47:51 +0200

    masked numbers

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,13 +4,27 @@ import mastercard from '../assets/images/mastercard.svg'
 import ankademin from '../assets/images/ankademin.png'
 
 const Card = ({ cardholder, cardNumber, expireMonth, expireYear, issuer }) => {
+  
+  const formatCardNumber = (cardnumber) => {
+    if (!cardNumber) return '';
+
+    // Ta bort befintliga mellanslag
+    const cleanNumber = cardNumber.replace(/\s+/g, ''); 
+  
+    // Visa de första 8 siffrorna och maskera de sista
+    const maskedNumber = cleanNumber.slice(0, 8) + ' **** ****';
+    
+    // Lägg till mellanslag för formatering
+    return maskedNumber.replace(/(\d{4})(?=\d)/g, "$1 ");
+  };
+
   return (
     <div className={`${styles.card} ${styles[issuer.toLowerCase()]}`}> {/* Dynamisk klass beroende på kortutgivare */}
       <div className={styles.cardContent}>
         <div className={styles.cardChip}></div>
         <div className={styles.cardDetails}> 
           <div className={styles.cardNumber}>
-            {cardNumber}
+            {formatCardNumber(cardNumber)}
           </div>
           <div className={styles.cardholder}>
             {cardholder.toUpperCase()}
